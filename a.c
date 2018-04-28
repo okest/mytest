7c18beaf87e3cb13ddabb55816c2ed77617748fe
okest <792185968@qq.com>
2018-04-28 22:20:29 +0800

    add a.c

--- a.c
+++ a.c
@@ -3,4 +3,8 @@
 void main()
 {
 	printf("---hello--world\n");
+	printf("-111--hello--world\n");
+	printf("-222--hello--world\n");
+	printf("-333--hello--world\n");
+	printf("-444--hello--world\n");
 }
